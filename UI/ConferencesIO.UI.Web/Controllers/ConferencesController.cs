080b77d0b9471286b3969ffd17880febb7f2971b
Rob Gibbens <RobGibbens@gmail.com>
2012-09-07 00:24:06 -0400

    Added Conferences and Apps site

---
+++ UI/ConferencesIO.UI.Web/Controllers/ConferencesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConferencesIO.UI.Web.Controllers
+{
+    public class ConferencesController : Controller
+    {
+        //
+        // GET: /Conferences/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
