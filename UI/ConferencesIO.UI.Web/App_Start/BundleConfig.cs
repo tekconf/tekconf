6f12f039a3fbfc2b6fb9996fe1b98e64e1f2bcfe
Rob Gibbens <RobGibbens@gmail.com>
2012-09-13 13:17:48 -0400

    Added some bundling

--- UI/ConferencesIO.UI.Web/App_Start/BundleConfig.cs
+++ UI/ConferencesIO.UI.Web/App_Start/BundleConfig.cs
@@ -8,36 +8,72 @@ namespace ConferencesIO.UI.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //            "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive*",
+            //            "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            //            "~/Content/themes/base/jquery.ui.core.css",
+            //            "~/Content/themes/base/jquery.ui.resizable.css",
+            //            "~/Content/themes/base/jquery.ui.selectable.css",
+            //            "~/Content/themes/base/jquery.ui.accordion.css",
+            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
+            //            "~/Content/themes/base/jquery.ui.button.css",
+            //            "~/Content/themes/base/jquery.ui.dialog.css",
+            //            "~/Content/themes/base/jquery.ui.slider.css",
+            //            "~/Content/themes/base/jquery.ui.tabs.css",
+            //            "~/Content/themes/base/jquery.ui.datepicker.css",
+            //            "~/Content/themes/base/jquery.ui.progressbar.css",
+            //            "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/css/home")
+                .Include(
+                            "~/css/bootstrap.css",
+                            "~/css/bootstrap-responsive.css",
+                            "~/css/font-awesome.css",
+                            "~/css/reboot-landing.css",
+                            "~/css/reboot-landing-responsive.css",
+                            "~/css/themes/green/theme.css",
+                            "~/css/pages/homepage.css"
+                        )
+                );
+
+            bundles.Add(new StyleBundle("~/css/conferences/index")
+                .Include(
+                            "~/css/bootstrap.css",
+                            "~/css/bootstrap-responsive.css",
+                            "~/css/font-awesome.css",
+                            "~/css/reboot-landing.css",
+                            "~/css/reboot-landing-responsive.css",
+                            "~/css/themes/green/theme.css",
+                            "~/css/pages/features.css"
+                        )
+                );
+            bundles.Add(new StyleBundle("~/css/conferences/detail")
+                .Include(
+                            "~/css/bootstrap.css",
+                            "~/css/bootstrap-responsive.css",
+                            "~/css/font-awesome.css",
+                            "~/css/reboot-landing.css",
+                            "~/css/reboot-landing-responsive.css",
+                            "~/css/themes/green/theme.css",
+                            "~/css/pages/about.css"
+                        )
+                );
+
         }
     }
 }
