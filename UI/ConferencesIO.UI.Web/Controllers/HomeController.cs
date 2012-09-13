84012e8fe561a7885f6bd9f5b577e73e72cf4193
Rob Gibbens <RobGibbens@gmail.com>
2012-09-13 15:17:31 -0400

    Optimizations gzip

--- UI/ConferencesIO.UI.Web/Controllers/HomeController.cs
+++ UI/ConferencesIO.UI.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Mvc;
+using ConferencesIO.UI.Web.App_Start;
 
 namespace ConferencesIO.UI.Web.Controllers
 {
   public class HomeController : Controller
   {
+      [CompressFilter]
     public ActionResult Index()
     {
       ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
