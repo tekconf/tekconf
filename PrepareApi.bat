e68776d817e90572d50468458d2ed1ae40fb81b5
Rob Gibbens <RobGibbens@gmail.com>
2012-09-17 23:41:48 -0400

    Added batch files

---
+++ PrepareApi.bat
@@ -0,0 +1,6 @@
+ren TekConf.Api.sln.temp TekConf.Api.sln
+ren TekConf.iOS.sln TekConf.iOS.sln.temp
+ren TekConf.UI.Mac.sln TekConf.UI.Mac.sln.temp
+ren TekConf.Web.sln TekConf.Web.sln.temp
+ren TekConf.WebAndApi.sln TekConf.WebAndApi.sln.temp
+ren TekConf.Win.sln TekConf.Win.sln.temp
---
+++ PrepareWeb.bat
@@ -0,0 +1,6 @@
+ren TekConf.Web.sln.temp TekConf.Web.sln
+ren TekConf.Api.sln TekConf.Api.sln.temp
+ren TekConf.iOS.sln TekConf.iOS.sln.temp
+ren TekConf.UI.Mac.sln TekConf.UI.Mac.sln.temp
+ren TekConf.WebAndApi.sln TekConf.WebAndApi.sln.temp
+ren TekConf.Win.sln TekConf.Win.sln.temp
