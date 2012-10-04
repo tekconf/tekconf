bc38510efc8b3d55fc728b4fb24fe501c215d59d
Rob Gibbens <RobGibbens@gmail.com>
2012-10-04 14:12:07 -0400

    Added admin page

--- RemoteData/Shared/v1/Dtos/FullConferenceDto.cs
+++ RemoteData/Shared/v1/Dtos/FullConferenceDto.cs
@@ -5,26 +5,29 @@ namespace TekConf.RemoteData.Dtos.v1
 {
     public class FullConferenceDto
     {
-        public string description { get; set; }
-        public string facebookUrl { get; set; }
         public string slug { get; set; }
+
+        public string name { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public string description { get; set; }
+        public string location { get; set; }
+        public string tagline { get; set; }
+        public string imageUrl { get; set; }
+        
+        public string facebookUrl { get; set; }
         public string homepageUrl { get; set; }
         public string lanyrdUrl { get; set; }
-        public string location { get; set; }
         public string meetupUrl { get; set; }
         public string googlePlusUrl { get; set; }
         public string vimeoUrl { get; set; }
         public string youtubeUrl { get; set; }
         public string githubUrl { get; set; }
         public string linkedInUrl { get; set; }
-        public string name { get; set; }
-        public DateTime start { get; set; }
-        public DateTime end { get; set; }
         public string twitterHashTag { get; set; }
         public string twitterName { get; set; }
+        
         public List<FullSessionDto> sessions { get; set; }
 
-        public string tagline { get; set; }
-        public string imageUrl { get; set; }
     }
 }
