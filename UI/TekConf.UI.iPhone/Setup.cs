041530fe8ff3c7203d4e29ec96150661657a4ac9
Rob Gibbens <RobGibbens@gmail.com>
2013-08-05 23:39:58 -0400

    iOS showing conferences list

--- UI/TekConf.UI.iPhone/Setup.cs
+++ UI/TekConf.UI.iPhone/Setup.cs
@@ -1,4 +1,5 @@
 using Cirrious.CrossCore;
+using Cirrious.MvvmCross.Binding;
 using Cirrious.MvvmCross.Plugins.Sqlite;
 using MonoTouch.UIKit;
 using Cirrious.CrossCore.Platform;
@@ -11,6 +12,32 @@ using TekConf.Core.Services;
 
 namespace TekConf.UI.iPhone
 {
+	public class iPhoneNetworkConnection : INetworkConnection
+	{
+		public bool IsNetworkConnected()
+		{
+			return true; //TODO
+		}
+
+		public string NetworkDownMessage
+		{
+			get
+			{
+				return "";
+			}
+		}
+	}
+
+	public class iPhoneMessageBox : IMessageBox
+	{
+		public void Show(string message)
+		{
+			var uiAlertView = new UIAlertView();
+			uiAlertView.Message = message;
+			uiAlertView.Show();
+		}
+	}
+
 	public class Setup : MvxTouchSetup
 	{
 		public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
@@ -25,14 +52,15 @@ namespace TekConf.UI.iPhone
 
 			Mvx.RegisterSingleton(typeof(IAuthentication), new Authentication(connection));
 			Mvx.RegisterSingleton<ISQLiteConnection>(connection);
-			//MvxBindingTrace.TraceBindingLevel = MvxTraceLevel.Diagnostic;
+			MvxBindingTrace.TraceBindingLevel = MvxTraceLevel.Diagnostic;
 			Mvx.RegisterType<IAnalytics, iPhoneAnalytics>();
 			Mvx.RegisterType<ICacheService, CacheService>();
 			Mvx.RegisterType<ILocalNotificationsRepository, LocalNotificationsRepository>();
 			Mvx.RegisterType<ILocalConferencesRepository, LocalConferencesRepository>();
 			Mvx.RegisterType<IRestService, RestService>();
 			Mvx.RegisterType<IPushSharpClient, PushSharpClient>();
-
+			Mvx.RegisterType<INetworkConnection, iPhoneNetworkConnection>();
+			Mvx.RegisterType<IMessageBox, iPhoneMessageBox>();
 			return new Core.App();
 		}
 
--- UI/TekConf.UI.iPhone/Views/ConferencesListView.cs
+++ UI/TekConf.UI.iPhone/Views/ConferencesListView.cs
@@ -1,29 +1,32 @@
-using System.Drawing;
 using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Binding.Touch.Views;
 using Cirrious.MvvmCross.Touch.Views;
-using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using TekConf.Core.ViewModels;
 
 namespace TekConf.UI.iPhone.Views
 {
 	[Register("ConferencesListView")]
-	public class ConferencesListView : MvxViewController
+	public class ConferencesListView : MvxTableViewController
 	{
+		public ConferencesListView()
+		{
+			Title = "Conferences";
+		}
+
 		public override void ViewDidLoad()
 		{
-			View = new UIView() { BackgroundColor = UIColor.White };
 			base.ViewDidLoad();
 
-			var label = new UISwitch(new RectangleF(10, 10, 300, 40));
-			Add(label);
-			var textField = new UITextField(new RectangleF(10, 50, 300, 40));
-			Add(textField);
+			var source = new MvxStandardTableViewSource(TableView, "TitleText name");
+			TableView.Source = source;
 
 			var set = this.CreateBindingSet<ConferencesListView, ConferencesListViewModel>();
-			set.Bind(label).To(vm => vm.IsAuthenticated);
-			//set.Bind(textField).To(vm => vm.Hello);
+			set.Bind(source).To(vm => vm.Conferences);
+			//set.Bind(source).For(s => s.SelectionChangedCommand).To(vm => vm.ShowDetailCommand);
 			set.Apply();
+
+			TableView.ReloadData();
 		}
 	}
 }
