267cb3d993e82d21d5c7deaee6bd439b3f9f57d5
Rob Gibbens <RobGibbens@arteksoftware.com>
2013-12-23 15:14:52 -0500

    Added LinkedIn Authentication

--- UI/TekConf.Web/Startup.cs
+++ UI/TekConf.Web/Startup.cs
@@ -3,45 +3,48 @@ using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Owin.Security.Providers.LinkedIn;
 using Owin.Security.Providers.Yahoo;
 
 [assembly: OwinStartup("WebStartup", typeof(TekConf.Web.Startup))]
 namespace TekConf.Web
 {
-    public partial class Startup 
-    {
-        public void Configuration(IAppBuilder app) 
-        {
-            //ConfigureAuth(app);
-            app.MapSignalR();
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			//ConfigureAuth(app);
+			app.MapSignalR();
 
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
-            });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString("/Account/Login")
+			});
+			// Use a cookie to temporarily store information about a user logging in with a third party login provider
+			app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-						app.UseMicrosoftAccountAuthentication(
-								clientId: "0000000040112827",
-								clientSecret: "dOusz11kRzfBWUShkNpbf465EPEbhRE5");
+			// Uncomment the following lines to enable logging in with third party login providers
+			app.UseMicrosoftAccountAuthentication(
+					clientId: "0000000040112827",
+					clientSecret: "dOusz11kRzfBWUShkNpbf465EPEbhRE5");
 
-            app.UseTwitterAuthentication(
-                    consumerKey: "3mYNb4Jt33Ttdgw4Q4Ppjw",
-                    consumerSecret: "RwRTOPu6tYoP2Yh0RBLOfQeWiTKPBjlfDv7IbNJ3G7k");
+			app.UseTwitterAuthentication(
+							consumerKey: "3mYNb4Jt33Ttdgw4Q4Ppjw",
+							consumerSecret: "RwRTOPu6tYoP2Yh0RBLOfQeWiTKPBjlfDv7IbNJ3G7k");
 
-            app.UseFacebookAuthentication(
-                    appId: "417883241605228",
-                    appSecret: "c2df6f0a2ed2a01f6f0553b3e58ad715");
+			app.UseFacebookAuthentication(
+							appId: "417883241605228",
+							appSecret: "c2df6f0a2ed2a01f6f0553b3e58ad715");
 
-	        app.UseYahooAuthentication(
-		        consumerKey:
-			        "dj0yJmk9QXpLck5Vc2ZQdnRUJmQ9WVdrOWRVNVRRVVpvTkhFbWNHbzlNVEEzT1RRMk5UTTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD1jMQ--",
-						consumerSecret: "224c0e0e5c3129e43b5c87273aa925a1ba089195");
+			app.UseYahooAuthentication(
+				consumerKey:
+					"dj0yJmk9QXpLck5Vc2ZQdnRUJmQ9WVdrOWRVNVRRVVpvTkhFbWNHbzlNVEEzT1RRMk5UTTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD1jMQ--",
+				consumerSecret: "224c0e0e5c3129e43b5c87273aa925a1ba089195");
 
-            app.UseGoogleAuthentication();
-        }
-    }
+			app.UseLinkedInAuthentication(clientId: "777d7tm6j83h3g", clientSecret: "qA05N3l3VldticeX");
+
+			app.UseGoogleAuthentication();
+		}
+	}
 }
