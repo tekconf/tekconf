a0d2948c8595496ef6b61fd1705b71c6389062ea
Rob Gibbens <RobGibbens@gmail.com>
2014-01-11 09:47:46 -0500

    Fixed OAuth, fixed font awesome

--- UI/TekConf.Web/assets/js/app.js
+++ UI/TekConf.Web/assets/js/app.js
@@ -42,14 +42,14 @@ var App = function () {
 
     function handleSearch() {    
         jQuery('.search').click(function () {
-            if(jQuery('.search-btn').hasClass('icon-search')){
+            if(jQuery('.search-btn').hasClass('fa-search')){
                 jQuery('.search-open').fadeIn(500);
-                jQuery('.search-btn').removeClass('icon-search');
-                jQuery('.search-btn').addClass('icon-remove');
+                jQuery('.search-btn').removeClass('fa-search');
+                jQuery('.search-btn').addClass('fa-remove');
             } else {
                 jQuery('.search-open').fadeOut(500);
-                jQuery('.search-btn').addClass('icon-search');
-                jQuery('.search-btn').removeClass('icon-remove');
+                jQuery('.search-btn').addClass('fa-search');
+                jQuery('.search-btn').removeClass('fa-remove');
             }   
         }); 
     }
