b7f1aa420a0ee173e5dc07893f8c092bb49ae1e3
Rob Gibbens <RobGibbens@gmail.com>
2016-09-12 18:04:02 -0400

    Added SmartAdmin

---
+++ src/tekconf.api/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TekConf.Api.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/content/smartadmin").IncludeDirectory("~/content/css", "*.min.css"));
+
+            bundles.Add(new ScriptBundle("~/scripts/smartadmin").Include(
+                "~/scripts/app.config.seed.min.js",
+                "~/scripts/bootstrap/bootstrap.min.js",
+                "~/scripts/app.seed.min.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- src/tekconf.api/Global.asax.cs
+++ src/tekconf.api/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using TekConf.Api.App_Start;
 using TekConf.Api.Infrastructure;
 
 namespace TekConf.Api
@@ -29,6 +31,7 @@ namespace TekConf.Api
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new FeatureViewLocationRazorViewEngine());
